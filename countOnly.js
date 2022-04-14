f304069f946cad50207416b4000cfbd3ef7c0156
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2022-04-14 01:52:01 -0600

    Create countOnly.js

---
+++ countOnly.js
@@ -0,0 +1,52 @@
+// assertEqual
+
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(` ✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual != expected) {
+    console.log(` 🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// Count Only Function
+/* 
+   allItems: an array of strings that we need to look through
+   itemsToCount: an object specifying what we need to count
+   */
+
+const countOnly = (allItems, itemstoCount) => {
+  const results = {};
+  for (let item of allItems) {
+    if (results[item]) {
+      results[item] += 1;
+    } else {
+      results[item] = 1;
+    }
+  }
+  return results;
+};
+
+// Tests
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe",
+];
+
+const result1 = countOnly(firstNames, {
+  Jason: true,
+  Karima: true,
+  Fang: true,
+  Agouhanna: false,
+});
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
