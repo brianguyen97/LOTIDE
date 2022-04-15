1c4591a0a8262380b5606a217668eb1f76f6742f
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2022-04-14 18:46:21 -0600

    Update Index.js

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const head = require('./head');
 const tail = require('./tail');
 const middle = require('./middle');
 const countLetters = require('./countLetters');
+const findKeyByValue = require('./findKeyByValue');
 
 module.exports = {
   head: head,
   tail: tail,
   middle: middle,
   countLetters: countLetters,
+  findKeyByValue: findKeyByValue,
 };
