ec2f88282ab1031f1cdbe158184c74df931a2a53
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2022-04-14 19:51:56 -0600

    Update letterPositions.js
    Removed tests, added to seperate file

--- letterPositions.js
+++ letterPositions.js
@@ -1,25 +1,3 @@
-// eqArrays Function
-const eqArrays = (array1, array2) => {
-  let trueOrFalse = true;
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] === array2[i]) {
-      trueOrFalse = true;
-    } else {
-      trueOrFalse = false;
-    }
-    return trueOrFalse;
-  }
-};
-
-// assertArraysEqual Function
-const assertArraysEqual = (array1, array2) => {
-  if (eqArrays(array1, array2)) {
-    console.log('✅ Arrays are equal. ✅');
-  } else {
-    console.log('🛑 Arrays are not equal. 🛑');
-  }
-};
-
 // letterPositions Function
 const letterPositions = inputString => {
   let results = {};
@@ -32,18 +10,10 @@ const letterPositions = inputString => {
   return results;
 };
 
-/* Expected Output
-  string = 'EILISH'
-  return object = {
-      E: [0],
-      I: [1, 3],
-      L: [2]
-      S: [4]
-      H: [5]
-  }
-  */
-
 // Tests
 
 console.log(letterPositions('EILISH'));
 console.log(letterPositions('RANDOM STRING WITH SPACES IN IT'));
+
+// Export
+module.exports = letterPositions;
