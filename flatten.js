a1b7ff5f6aa6654135711fe226c3928220aa04b3
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2022-04-14 22:42:31 -0600

    Update flatten.js
    Remove eqArrays and assertArraysEqual

--- flatten.js
+++ flatten.js
@@ -1,29 +1,4 @@
-// eqArrays Function
-
-const eqArrays = (array1, array2) => {
-  let trueOrFalse = true;
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] === array2[i]) {
-      trueOrFalse = true;
-    } else {
-      trueOrFalse = false;
-    }
-    return trueOrFalse;
-  }
-};
-
-// assertArraysEqual Function
-
-const assertArraysEqual = (array1, array2) => {
-  if (eqArrays(array1, array2)) {
-    console.log('✅ Arrays are equal. ✅');
-  } else {
-    console.log('🛑 Arrays are not equal. 🛑');
-  }
-};
-
 // Flatten Function
-
 const flatten = function (arr) {
   let result = [];
   for (let i = 0; i < arr.length; i++) {
@@ -35,3 +10,6 @@ const flatten = function (arr) {
   }
   return result;
 };
+
+// Export
+module.exports = flatten;
